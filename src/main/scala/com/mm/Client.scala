d0535176d72159023cea41bdc24f349beb8e5679
muszynskim <MuszynskiM@quindell.com>
2016-09-12 18:22:26 +0100

    simple client for loan creation

---
+++ src/main/scala/com/mm/Client.scala
@@ -0,0 +1,55 @@
+package com.mm
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.OutgoingConnection
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object Client {
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  def main(args: Array[String]): Unit = {
+//     doHello(system)
+     doLoanCreation()
+    }
+
+  def doLoanCreation(): Unit = {
+
+    val requestEntity = HttpEntity(ContentTypes.`application/json`,"{\"amount\": 145}")
+
+    val connectionFlow: Flow[HttpRequest, HttpResponse, Future[OutgoingConnection]] =
+      Http().outgoingConnection("127.0.0.1", 8080)
+    val responseFuture: Future[HttpResponse] =
+      Source.single(HttpRequest(method = HttpMethods.POST, uri = "/loans", entity=requestEntity))
+        .via(connectionFlow)
+        .runWith(Sink.head)
+    responseFuture.andThen {
+      case Success(x) => println(s"request succeded: $x")
+      case Failure(_) => println("request failed")
+    }.andThen {
+      case _ => system.terminate()
+    }
+  }
+
+
+  def doHello(): Unit = {
+    val connectionFlow: Flow[HttpRequest, HttpResponse, Future[OutgoingConnection]] =
+      Http().outgoingConnection("127.0.0.1", 8080)
+    val responseFuture: Future[HttpResponse] =
+      Source.single(HttpRequest(uri = "/hello"))
+        .via(connectionFlow)
+        .runWith(Sink.head)
+    responseFuture.andThen {
+      case Success(x) => println(s"request succeded: $x")
+      case Failure(_) => println("request failed")
+    }.andThen {
+      case _ => system.terminate()
+    }
+  }
+}
--- src/main/scala/com/mm/WebServer.scala
+++ src/main/scala/com/mm/WebServer.scala
@@ -1,23 +1,46 @@
 package com.mm
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
 
+import scala.concurrent.Future
 import scala.io.StdIn
 
+
+
+
 object WebServer {
+  final case class Loan(amount:Int)
+  def saveLoan(loan:Loan):Future[Done] = Future.successful(Done)
+  implicit val loanFormat = jsonFormat1(Loan)
+
+
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem("mm-actor-system")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
 
+
     val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,"<h1>Hello Akka-Http</h1>"))
+      get {
+        pathPrefix("hello") {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,"<h1>Hello Akka-Http</h1>"))
+        }
+      } ~
+      post {
+        pathPrefix("loans") {
+          entity(as[Loan]) { loan =>
+            val saved: Future[Done] = saveLoan(loan)
+            onComplete(saved) { done =>
+              complete(s"loan created for ${loan.amount}")
+            }
+          }
         }
       }
 
